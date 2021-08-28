993f82b0127377cb0dec0e976c11dd3573e04529
sumedha60 <76279963+sumedha60@users.noreply.github.com>
2021-08-28 10:27:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,87 +1,72 @@
-Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
-var particles = [];
-var plinkos = [];
-var divisions =[];
-var particle;
-var divisionHeight=300;
-var score =0;
-function setup() {
-  createCanvas(800, 800);
-  engine = Engine.create();
-  world = engine.world;
-  ground = new Ground(width/2,height,width,20);
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-  //create division objects
-  for (var k = 0; k <=850; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 5, divisionHeight));
-  }
-  
-  //create 1st row of plinko objects
-  for (var j = 75; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
+var engine, world;
+var backgroundImg;
+var hour;
 
-  //create 2nd row of plinko objects
-  for (var j = 50; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,175));
-  }
-
-  //create 3rd row of plinko objects
-  for (var j = 100; j <=width-60; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,275));
-  }
-
-  
-  //create 4th row of plinko objects
-  for (var j =170; j <=width-80; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,375));
-  }
-
-
-  //create particle objects
-  
-  //particle=new Particle(400,300)
-    
-  }
- 
-
-
-function draw() {
-  background("black");
-  textSize(20)
- 
-  Engine.update(engine);
-  ground.display();
-  
-  if(frameCount%60===0){
-    particles.push(new Particle(random(0,800),0))
-  }
-  //console.log(particles)
-  //display the plinkos 
-  for (var i = 0; i < plinkos.length; i++) {
-    plinkos[i].display();   
-  }
-   
-  //display the divisions
-  for (var k = 0; k < divisions.length; k++) {
-    divisions[k].display();
-  }
-  //for (var l = 140; l < divisions.length; l++) {
-   // divisions[l].display();
- // }
-
-  //display the paricles 
-for (var j = 0;j < particles.length;j++){
-  particles[j].display(); 
-  //console.log(particles[j])
+var bg = "sunrise.png";
+var bg = "sunset.png"
+function preload() {
+    getBackgroundImg();
+    //getTime()
 }
-//console.log(particles)
-  //particle.display();
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+
+    Engine.update(engine);
+
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+    }
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response= await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    console.log(response)
+
+    //change the data in JSON format and store it in variable responseJSON
+    var responseJSON= await response.json()
+    console.log(responseJSON)
+
+    //fetch datetime from responseJSON
+    var datetime= responseJSON.datetime
+    
+    // slice the datetime to extract hour
+     hour= datetime.slice(11,13)
+    console.log(hour)
+    var bg 
+    
+    if(hour>6 && hour<7 ){
+        bg = "sunrise.png";
+    }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
+    
+   // getBackgroundImg
+   console.log(datetime)
 }
